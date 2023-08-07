6bb700f923823eafeec59fb5c92272275a4c4b5f
Thabo <thabo.radebe@live.com>
2023-08-08 00:56:29 +0200

    newmembers

--- src/components/UI/person/MemberItem.js
+++ src/components/UI/person/MemberItem.js
@@ -22,45 +22,47 @@ const MemberItem = (props) => {
     }
   };
   return (
-    <li>
-      <Card className={classes["expense-item"]}>
-        <ExpenseDate img={props.img} />
-        <div className={classes["expense-item__description"]}>
-          <div className={classes["discription"]}>
-            <div>Title: {props.title}</div>
-            <div>Surname: {props.surname}</div>
-            <div className={classes.price}>Branch: {props.branch}</div>
-          </div>
-          <div className={classes["discription"]}>
-            <div>Cell: {props.cell}</div>
-            <div>ID: {props.idNumber}</div>
-            <div className={classes.price}>Birthday: {props.Birthday}</div>
-          </div>
-          <div className={classes["discription"]}>
-            <div>Active: {props.Active ? "Yes" : "No"}</div>
-            <div>Sealed: {props.sealed ? "Yes" : "No"}</div>
-            <div className={classes.price}>
-              Year/s of Service: {props.serviceYears}
+    <div>
+      <li>
+        <Card className={classes["expense-item"]}>
+          <ExpenseDate img={props.img} />
+          <div className={classes["expense-item__description"]}>
+            <div className={classes["discription"]}>
+              <div>Title: {props.title}</div>
+              <div>Surname: {props.surname}</div>
+              <div className={classes.price}>Branch: {props.branch}</div>
+            </div>
+            <div className={classes["discription"]}>
+              <div>Cell: {props.cell}</div>
+              <div>ID: {props.idNumber}</div>
+              <div className={classes.price}>Birthday: {props.Birthday}</div>
+            </div>
+            <div className={classes["discription"]}>
+              <div>Active: {props.Active ? "Yes" : "No"}</div>
+              <div>Sealed: {props.sealed ? "Yes" : "No"}</div>
+              <div className={classes.price}>
+                Year/s of Service: {props.serviceYears}
+              </div>
+            </div>
+            <div className={classes["button_ud"]}>
+              <Link
+                className={classes["expense-item__pressent"]}
+                to={`/updateMember/ ${props.id}`}
+              >
+                Edit
+              </Link>
+              <button
+                className={classes["expense-item__absent"]}
+                onClick={deleteHanler}
+              >
+                Delete
+              </button>
             </div>
           </div>
-          <div className={classes["button_ud"]}>
-            <Link
-              className={classes["expense-item__pressent"]}
-              to={`/updateMember/ ${props.id}`}
-            >
-              Edit
-            </Link>
-            <button
-              className={classes["expense-item__absent"]}
-              onClick={deleteHanler}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      </Card>
-      {showDeleteSmg && <Delete onClose={hideDeleteHandler} />}{" "}
-    </li>
+        </Card>
+        {showDeleteSmg && <Delete onClose={hideDeleteHandler} />}{" "}
+      </li>
+    </div>
   );
 };
 
