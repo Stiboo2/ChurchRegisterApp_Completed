26c36f34745941acf51a0f726607aedad3c12af9
Thabo <thabo.radebe@live.com>
2023-08-08 11:27:53 +0200

    dataToCalcutale

--- src/components/UI/person/Calculated.js
+++ src/components/UI/person/Calculated.js
@@ -1,5 +1,31 @@
+import React, { useEffect } from "react";
 import classes from "./Calculated.module.css";
-const Calculated = () => {
-  return <div className={classes.calculatedCard}>Calculated</div>;
+
+const Calculated = ({
+  sealedFalseCount,
+  CountMembers,
+  branchCounts,
+  titleCounts,
+}) => {
+  useEffect(() => {
+    console.log(
+      "Number of members with sealed=false:",
+      sealedFalseCount,
+      CountMembers
+    );
+    console.log("Branch Counts:", branchCounts);
+    console.log("Branch Title:", titleCounts);
+  }, []);
+
+  return (
+    <div className={classes.calculatedCard}>
+      <p>
+        Number of members with sealed=false: {sealedFalseCount}, {CountMembers}
+      </p>
+      <p>Branch Counts: {JSON.stringify(branchCounts)}</p>
+      <p>Branch Title: {JSON.stringify(titleCounts)}</p>
+    </div>
+  );
 };
+
 export default Calculated;
